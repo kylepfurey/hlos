93b51fc769e701c74ceb727bbf3c1946e893354e
Kyle Furey <kylepfurey@gmail.com>
2025-11-02 14:41:08 -0500

    Typo

--- boot/boot.asm
+++ boot/boot.asm
@@ -130,9 +130,9 @@ gdt_descriptor:
 
 ; Global descriptor table
 gdt_start:
-	dq 0x0000000000000000			; Null segment
-	dq 0x00CF9A000000FFFF			; Code segment
-	dq 0x00CF92000000FFFF			; Data segment
+	dq 0x0							; Null segment
+	dq 0xCF9A000000FFFF				; Code segment
+	dq 0xCF92000000FFFF				; Data segment
 gdt_end:
 
 
