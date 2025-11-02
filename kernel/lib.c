e2bde2214cbf38a3446f84f7a84d70aa1980f09b
Kyle Furey <kylepfurey@gmail.com>
2025-11-01 21:01:16 -0400

    lib.h Implemented

--- kernel/lib.c
+++ kernel/lib.c
@@ -57,7 +57,21 @@ uint_t hash(const void* data, uint_t size) {
 void* copy(void* dest, const void* src, uint_t size) {
 	assert(dest != NULL, "copy() - dest was NULL!");
 	assert(src != NULL, "copy() - src was NULL!");
-	// TODO
+	byte_t* destbytes = (byte_t*)dest;
+	const byte_t* srcbytes = (const byte_t*)src;
+	if (destbytes == srcbytes || size == 0) {
+		return dest;
+	}
+	if (destbytes < srcbytes || destbytes >= srcbytes + size) {
+		for (uint_t i = 0; i < size; ++i) {
+			destbytes[i] = srcbytes[i];
+		}
+	}
+	else {
+		for (uint_t i = size; i > 0; --i) {
+			destbytes[i - 1] = srcbytes[i - 1];
+		}
+	}
 	return dest;
 }
 
@@ -68,14 +82,26 @@ void* copy(void* dest, const void* src, uint_t size) {
 comparison_t compare(const void* left, const void* right, uint_t size) {
 	assert(left != NULL, "compare() - left was NULL!");
 	assert(right != NULL, "compare() - right was NULL!");
-	// TODO
+	const byte_t* leftbytes = (const byte_t*)left;
+	const byte_t* rightbytes = (const byte_t*)right;
+	for (uint_t i = 0; i < size; ++i) {
+		if (leftbytes[i] < rightbytes[i]) {
+			return LESS_THAN;
+		}
+		if (leftbytes[i] > rightbytes[i]) {
+			return GREATER_THAN;
+		}
+	}
 	return EQUAL_TO;
 }
 
 /** Sets each byte of <dest> to <num> for <size>. Returns <dest>. */
 void* set(void* dest, byte_t num, uint_t size) {
 	assert(dest != NULL, "set() - dest was NULL!");
-	// TODO
+	byte_t* bytes = (byte_t*)dest;
+	while (size-- > 0) {
+		bytes[size] = num;
+	}
 	return dest;
 }
 
@@ -83,28 +109,51 @@ void* set(void* dest, byte_t num, uint_t size) {
 void* swap(void* left, void* right, uint_t size) {
 	assert(left != NULL, "swap() - left was NULL!");
 	assert(right != NULL, "swap() - right was NULL!");
-	// TODO
+	byte_t* leftbytes = (byte_t*)left;
+	byte_t* rightbytes = (byte_t*)right;
+	while (size-- > 0) {
+		byte_t temp = leftbytes[size];
+		leftbytes[size] = rightbytes[size];
+		rightbytes[size] = temp;
+	}
 	return left;
 }
 
 /** Reverses <size> number of bytes in <src>. Returns <src>. */
 void* flip(void* src, uint_t size) {
 	assert(src != NULL, "flip() - src was NULL!");
-	// TODO
+	byte_t* bytes = (byte_t*)src;
+	uint_t count = size / 2;
+	for (uint_t i = 0; i < count; ++i) {
+		byte_t temp = bytes[i];
+		bytes[i] = bytes[size - 1 - i];
+		bytes[size - 1 - i] = temp;
+	}
 	return src;
 }
 
 /** Finds the first matching byte equal to <num> for <size> and returns its index (or NOT_FOUND). */
 uint_t first(const void* src, byte_t num, uint_t size) {
 	assert(src != NULL, "first() - src was NULL!");
-	// TODO
+	const byte_t* bytes = (const byte_t*)src;
+	for (uint_t i = 0; i < size; ++i) {
+		if (bytes[i] == num) {
+			return i;
+		}
+	}
 	return NOT_FOUND;
 }
 
 /** Finds the last matching byte equal to <num> for <size> and returns its index (or NOT_FOUND). */
 uint_t last(const void* src, byte_t num, uint_t size) {
 	assert(src != NULL, "last() - src was NULL!");
-	// TODO
+	const byte_t* bytes = (const byte_t*)src;
+	uint_t i = size;
+	while (i-- > 0) {
+		if (bytes[i] == num) {
+			return i;
+		}
+	}
 	return NOT_FOUND;
 }
 
@@ -115,7 +164,20 @@ uint_t last(const void* src, byte_t num, uint_t size) {
 uint_t find(const void* dest, uint_t size1, const void* src, uint_t size2) {
 	assert(dest != NULL, "find() - dest was NULL!");
 	assert(src != NULL, "find() - src was NULL!");
-	// TODO
+	if (size2 == 0 || size2 > size1) {
+		return NOT_FOUND;
+	}
+	const byte_t* destbytes = (const byte_t*)dest;
+	const byte_t* srcbytes = (const byte_t*)src;
+	for (uint_t i = 0; i <= size1 - size2; ++i) {
+		uint_t j = 0;
+		while (j < size2 && destbytes[i + j] == srcbytes[j]) {
+			++j;
+		}
+		if (j == size2) {
+			return i;
+		}
+	}
 	return NOT_FOUND;
 }
 
